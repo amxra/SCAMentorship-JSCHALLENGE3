4f9895068e089c67e6343a9310a6e89a8a991687
Adebimpe <realadebimpe@gmail.com>
2020-10-23 22:53:14 +0100

    Modified Challenge3

--- Challenge3.js
+++ Challenge3.js
@@ -15,3 +15,14 @@ Output Example:
 
 Put your answer below! This would be discussed in our weekly meeting on Saturday! Please complete and hand in before then so i can review for feedback! Complete feedback would only be given to those at the meeting on saturday! Take care :) */
 
+function reverseNumber(str) {
+    let arr;
+    arr = str.split("");
+    let reversedString = [];
+    
+    for(let i = 1; i <= arr.length; i++){
+     reversedString.push(arr[arr.length - i])
+    }
+    return reversedString.join("");
+  }
+  reverseNumber("12345");
